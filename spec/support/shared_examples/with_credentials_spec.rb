5422f6300d6587650982713344605fb8f64ebd71
mebobby2 <bobl53@gmail.com>
2016-04-29 10:42:43 +1200

    make spec more robust

--- spec/support/shared_examples/with_credentials_spec.rb
+++ spec/support/shared_examples/with_credentials_spec.rb
@@ -5,13 +5,17 @@ shared_examples_for "a resource action with credentials" do
     described_action = example.metadata[:example_group][:parent_example_group][:parent_example_group][:description_args].first.gsub("#", "")
 
     it "throws an missing sendle_id error" do
+      Sendle::Api.sendle_id = nil
+      Sendle::Api.api_key = 'fake-key'
+
       expect {
         described_resource.send(described_action)
       }.to raise_error Sendle::Api::Errors::MissingSendleId
     end
 
     it "throws an missing api_key error" do
-      Sendle::Api.sendle_id = 'sdfsfdsf'
+      Sendle::Api.sendle_id = 'fake-id'
+      Sendle::Api.api_key = nil
 
       expect {
         described_resource.send(described_action)
