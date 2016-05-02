02737598284ce629da0b6acca4ada83939370e7c
mebobby2 <bobl53@gmail.com>
2016-05-02 15:32:59 +1200

    more refactorings

--- lib/sendle/api/actions/base.rb
+++ lib/sendle/api/actions/base.rb
@@ -3,22 +3,53 @@ module Sendle
     module Actions
       module Base
 
-        def check_for_missing_credentials
-          return unless self.include_credentials?
-          raise Sendle::Api::Errors::MissingSendleId if Sendle::Api.sendle_id.nil?
-          raise Sendle::Api::Errors::MissingApiKey if Sendle::Api.api_key.nil?
+        def request(params)
+          RestClient::Request.execute(rest_client_params(params))
         end
 
-        def json_headers
-          { accept: :json, content_type: :json }
-        end
+        protected
 
-        def credential_params
-          {
-            user: Sendle::Api.sendle_id,
-            password: Sendle::Api.api_key
-          }
-        end
+          #Hook method
+          def http_method
+            :get
+          end
+
+        private 
+
+          def rest_client_params(params)
+            rc_params = include_credentials? ? common_params_with_credentials(params) : common_params(params)
+            rc_params.merge(url: url)
+          end
+
+          def common_params(params)
+            headers = json_headers
+            headers.merge!(params: params) unless params.empty?
+            {
+              method: http_method,
+              headers: headers
+            }
+          end
+
+          def common_params_with_credentials(params)
+            common_params(params).merge(credential_params)
+          end
+
+          def check_for_missing_credentials
+            return unless self.include_credentials?
+            raise Sendle::Api::Errors::MissingSendleId if Sendle::Api.sendle_id.nil?
+            raise Sendle::Api::Errors::MissingApiKey if Sendle::Api.api_key.nil?
+          end
+
+          def json_headers
+            { accept: :json, content_type: :json }
+          end
+
+          def credential_params
+            {
+              user: Sendle::Api.sendle_id,
+              password: Sendle::Api.api_key
+            }
+          end
 
       end
     end
--- lib/sendle/api/actions/index.rb
+++ lib/sendle/api/actions/index.rb
@@ -6,9 +6,7 @@ module Sendle
         def index(params = {})
           check_for_missing_credentials if include_credentials?
           validate_index_request(params) 
-
-          response = RestClient::Request.execute(rest_client_params(params))
-          process_index_response(response)
+          process_index_response(request(params))
         rescue RestClient::Unauthorized, RestClient::PaymentRequired, RestClient::UnprocessableEntity => e 
           raise Sendle::Api::Factories::Errors.new_error(e)
         end
@@ -16,26 +14,6 @@ module Sendle
         def self.included(base)
           base.extend(Sendle::Api::Sugars::Index)
         end
-
-        private
-
-          def rest_client_params(params)
-            rc_params = include_credentials? ? common_params_with_credentials(params) : common_params(params)
-            rc_params.merge(url: url)
-          end
-
-          def common_params(params)
-            headers = json_headers
-            headers.merge!(params: params) unless params.empty?
-            {
-              method: :get,
-              headers: headers
-            }
-          end
-
-          def common_params_with_credentials(params)
-            common_params(params).merge(credential_params)
-          end
 
       end
     end
