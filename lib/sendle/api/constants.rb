7af2b01ab34b51dab0485120505bf2dbad10d5e0
mebobby2 <bobl53@gmail.com>
2016-05-04 09:14:02 +1200

    added order statues to constants

--- lib/sendle/api/constants.rb
+++ lib/sendle/api/constants.rb
@@ -4,6 +4,18 @@ module Sendle
     PLAN_PREMIUM = 'premium'
     PLAN_PRO = 'pro'
     PLANS = [ PLAN_EASY, PLAN_PREMIUM, PLAN_PRO ]
+
+    ORDER_STATUS_PAYMENT = 'Payment'
+    ORDER_STATUS_PICKUP = 'Pickup'
+    ORDER_STATUS_IN_TRANSIT = 'In Transit'
+    ORDER_STATUS_DELIVERED = 'Delivered'
+    ORDER_STATUS_CANCELLED = 'Cancelled'
+    ORDER_STATUSES = [ ORDER_STATUS_PAYMENT,
+                       ORDER_STATUS_PICKUP,
+                       ORDER_STATUS_IN_TRANSIT,
+                       ORDER_STATUS_DELIVERED,
+                       ORDER_STATUS_CANCELLED 
+                      ]
   end
 end
 
